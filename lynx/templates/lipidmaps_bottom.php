5ae6c28ca1ea0aae49e138ca90e9211d12e9de92
cgaud <caroline.gaud@free.fr>
2020-10-05 16:26:17 +0100

    Updates to deploy LipidLynxX on LIPID MAPS. Add LIPID MAPS template - update some CSS

---
+++ lynx/templates/lipidmaps_bottom.php
@@ -0,0 +1,103 @@
+</div>
+
+<footer class="bg-dark" style="padding-top:1rem;">
+    <div class="footer-bottom bg-light text-muted" style='padding-top:0.4rem; padding-bottom:0rem; text-align: center;'>
+        <div class="container">
+            <div class="row">
+                <div class="col" style='text-align: center;'>
+                    <p style='font-size:1.6rem;'>Participating Institutions</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class='bg-light' style='padding-top:0.4rem;'>
+        <div class="container">
+            <div class="row align-items-end" style='border-bottom-style:solid; border-color: #cccccc; padding-bottom:2rem;'>
+                <div class="col-sm" style='text-align:center; padding:1rem;'>
+                    <img src="/images/cardiff_logo.png" style="width: 4rem;" alt="Cardiff University logo">
+                    <br><a class="links" href='https://www.cardiff.ac.uk'>Cardiff University</a>
+                </div>
+                <div class="col-sm" style='text-align:center; padding:1rem;'>
+                    <img src="/images/BI-2016-small.png" style="width: 4.9rem;" alt="Babraham Institutei logo">
+                    <br><a class="links" href='https://www.babraham.ac.uk/'>Babraham Institute</a>
+                </div>
+                <div class="col-sm" style='text-align:center; padding:1rem;'>
+                    <img src="/images/UCSanDiegoLogo-BlueGold.png" style="width: 150px;" alt="University of California, San Diego logo">
+                    <br><a class="links" href='https://www.ucsd.edu'>University of California, San Diego</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bottom bg-light text-muted" style='margin-top:0.0rem; padding-top:1.4rem; padding-bottom:0rem; text-align: center;'>
+        <div class="container">
+            <div class="row">
+                <div class="col" style='text-align: center;'>
+                    <p style='font-size:1.6rem;'>Funded by</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bottom bg-light text-muted" style='padding-top:0rem; text-align: center;'>
+        <div class="container">
+            <div class="row">
+                <div class="col" style='text-align: center;'>
+                    <p>
+                        <img src="/images/wellcome-logo-black.jpg" style="width: 4rem;" alt="Wellcome Trust logo">
+                        <br>
+                        <a class="links" href='https://wellcome.ac.uk/'>Wellcome Trust</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="m-t-3">
+    </div>
+
+    <div class="footer-top p-y-2">
+        <div class="container-fluid">
+        </div>
+    </div>
+
+    <div class="footer-bottom text-muted" style='background-color:black; padding-top:0.4rem; padding-right:0rem;'>
+        <div class="container" style='width:100%;'>
+            <div class="row" style='width:100%;'>
+
+                <div class="col-sm-4">
+                    <span class='text-muted small'>
+                        &copy; 2003-2020 LIPID MAPS® Lipidomics Gateway
+                    </span>
+                </div>
+
+                <div class="col-sm-2">
+                    <span class='text-muted small'>
+                        <a href="http://www.w3.org/html/logo/" class="no-icon">
+                            <img src="https://www.w3.org/html/logo/badge/html5-badge-h-css3-semantics.png" height='30' alt="HTML5 Powered with CSS3 / Styling, and Semantics" title="HTML5 Powered with CSS3 / Styling, and Semantics">
+                        </a>
+                    </span>
+                </div>
+
+
+                <div class="col-sm">
+                    <ul class="list-inline text-muted small">
+                        <li class="list-inline-item"><a class="links" href="/about/termsofuse.php">Terms of Use</a></li>
+                        <li class="list-inline-item"><a class="links" href="/about/howtocite.php">How to Cite</a></li>
+                        <li class="list-inline-item"><a class="links" href="/about/howtolink.php">How to Link</a></li>
+                        <li class="list-inline-item"><a class="links" href="/about/browsers.php">Browsers Supported</a></li>
+                        <li class="list-inline-item"><a class="links" href="/about/cookies.php">Manage Cookies</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+<script type="text/javascript" src="/js/components.js"></script>
+</body>
+</html>
---
+++ lynx/templates/lipidmaps_top.php
@@ -0,0 +1,95 @@
+<!-- View -->
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+	<head>
+		<title>LIPID MAPS® Lipidomics Gateway</title>
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,800">
+		<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+        <script type='text/javascript' src='https://www.google.com/recaptcha/api.js'></script>
+
+		<link rel="stylesheet" href="/css/carousel_24_20190225a.css">
+		<link rel="stylesheet" href="/css/bootstrap_20190227c.css">
+		<link rel="stylesheet" href="/css/components.css">
+		<link href="/css/sticky_footer.css" rel="stylesheet">
+		<link rel="stylesheet" href="/css/cookiealert.css">
+
+		<style>
+			.links{text-decoration:underline; color:#1F5CAA;}	
+		</style>
+	</head>
+
+	<body style='padding:0rem; margin:0rem;'>
+		<nav class="navbar navbar-expand-xl navbar-dark bg-dark" style="margin:0rem;">
+			<!-- Nav Logo -->
+			<a class="navbar-brand" href="/">
+				<img src="/images/logo_R_transparent.png" width="60" class="d-inline-block align-top" alt="logo">
+				<span class="d-none d-md-inline" style="font-size:1.6rem; padding-left:0.6rem;">
+					LIPID MAPS® Lipidomics Gateway
+				</span>
+			</a>
+
+			<!-- Nav Toggler -->
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<!-- Nav Menu -->
+			<div class="collapse navbar-collapse" id="navbarToggler">
+				<ul class="navbar-nav ml-auto">
+					<!-- Home -->
+					<li class="nav-item">
+						<a class="nav-link" href="/">Home</a>
+					</li>
+					<!-- Updates -->
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="" id="navbarDropdownUpdates" data-toggle="lm-dropdown">Updates</a>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="/updates/eventscalendar.php">Events Calendar</a>
+							<a class="dropdown-item" href="/updates/lipidmapshighlights.php">LIPID MAPS® Highlights</a>
+							<a class="dropdown-item" href="/resources/lipidweb/index.php?page=info/blog.htm">Lipid Matters (blog)</a>
+							<a class="dropdown-item" href="/updates/lipidofthemonth.php">Lipid of the Month</a>
+						</div>
+					</li>
+					<!-- Resources -->
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="" id="navbarDropdownResources" data-toggle="lm-dropdown">Resources</a>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="/data/classification/LM_classification_exp.php">Classification</a>
+							<a class="dropdown-item" href="/resources/standards/index.php">Standards</a>
+							<a class="dropdown-item" href="/resources/protocols/index.php">Methods and Protocols</a>
+							<a class="dropdown-item" href="/resources/data/index.php">Experimental Data</a>
+							<a class="dropdown-item" href="/resources/databases/index.php">Databases</a>
+							<a class="dropdown-item" href="/resources/pathways/index.php">Pathways</a>
+							<a class="dropdown-item" href="/resources/REST/index.php">REST Service</a>
+							<a class="dropdown-item" href="/resources/tools/index.php">Tools</a>
+							<a class="dropdown-item" href="/resources/tutorials/index.php">Tutorials and Webinars</a>
+							<a class="dropdown-item" href="/resources/lipidweb/index.php">LipidWeb</a>
+							<a class="dropdown-item" href="/resources/links/index.php">Links</a>
+						</div>
+					</li>
+					<!-- About -->
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="" id="navbarDropdownAbout" data-toggle="lm-dropdown">About</a>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="/about/index.php">About Us</a>
+							<a class="dropdown-item" href="/about/partners.php">About Our Academic Partners</a>
+							<a class="dropdown-item" href="/about/sponsors.php">About Our Sponsors</a>
+							<a class="dropdown-item" href="/about/sponsor_us.php">Sponsor Us</a>
+							<a class="dropdown-item" href="/about/contact.php">Contact Us</a>
+							<a class="dropdown-item" href="/about/archive.php">Archive</a>
+						</div>
+					</li>
+				</ul>
+				<!-- Search Field -->
+				<form class="form-inline" action='/search/quicksearch.php' method="get">
+					<input class="form-control mr-sm-2" name="Name" type="search" placeholder="Search" aria-label="Search">
+					<div class="input-group-btn">
+						<button class="btn btn-default" type="submit" style='background-color:#ededed;'><i class="fas fa-search"></i></button>
+					</div>
+				</form>
+			</div>
+		</nav>
+
+		<div>
